3381d60624e7bca81253ede5189b3188b80f115c
Borys-Grinberg <borys.grinberg@gmail.com>
2024-02-10 00:35:58 +0100

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+import React from 'react';
+import Profile from 'components/Profile';
+import user from 'components/user.json';
+ export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Bububu
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import './components/user.css';
+import './components/data.css';
+import './components/friends.css';
+import './components/transactions.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
